eb99317d4ef972cadd7f46d394aa0e02034b1fc7
lifeiyan <1553652636@qq.com>
2018-06-10 19:46:13 +0800

    修改登录一些内容

---
+++ 小说/NOVDataModel.h
@@ -0,0 +1,13 @@
+//
+//  NOVDataModel.h
+//  小说
+//
+//  Created by 李飞艳 on 2018/6/9.
+//  Copyright © 2018年 李飞艳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NOVDataModel : NSObject
+
+@end
---
+++ 小说/NOVDataModel.m
@@ -0,0 +1,13 @@
+//
+//  NOVDataModel.m
+//  小说
+//
+//  Created by 李飞艳 on 2018/6/9.
+//  Copyright © 2018年 李飞艳. All rights reserved.
+//
+
+#import "NOVDataModel.h"
+
+@implementation NOVDataModel
+
+@end
